9df4b3c4d17b847464709b4a0f9c3f1af54659be
Ubuntu <cjardin@jardinlabs.com>
2021-10-08 20:23:03 +0000

    End of Lecture

--- coin_miner/my_miner.py
+++ coin_miner/my_miner.py
@@ -2,26 +2,54 @@ import hashlib
 from itertools import product
 import binascii
 
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool #uses processors
 
-#print(hashlib.sha256("heeelllo1".encode('utf-8')).hexdigest())
 
-#print(hashlib.sha256("heeelllo1".encode('utf-8')).digest()[1])
 
-perm = product(list(range(256)), repeat=2)
+#problem space = all possible search points 
+jobs_to_do = []
 
-MODS = 10
-MY_MOD = 2
+def min_coin(x):
+    repeat = x['repeat'] #how man bytes
+    mod_number = x['mod_number'] #how man processers to run
+    mod_pos = x['mod_pos'] # which processor am I
+    ones = x['ones'] # the number of ones to search for
 
-count = 0
-for p in perm:
+    looking_for = [1] * ones
+    count=0
+    perm = product(list(range(256)), repeat=repeat)
+    coins_found = []
+    for p in perm:
+        #print(p)
+        if (count % mod_number) == mod_pos:
+            coin_hash = hashlib.sha256(bytes( p )).digest()
+            if list(coin_hash[0:ones]) == looking_for:
+                coins_found.append(p)
 
-    count += 1
-    if count % MODS != MY_MOD:
-        continue 
-    print(binascii.hexlify(bytearray(p)))
-    #coin_hash = hashlib.sha256(bytes( p )).digest()
-    #if coin_hash[0] == 1 and coin_hash[1] == 1:
-    #    print("found one")
+    return coins_found
+
+
+
+
+jobs_to_do.append( {'repeat' :2, 'mod_number': 2, 'mod_pos': 1, 'ones': 2} ) 
+jobs_to_do.append( {'repeat' :2, 'mod_number': 2, 'mod_pos': 0, 'ones': 2} )
+jobs_to_do.append( {'repeat' :2, 'mod_number': 2, 'mod_pos': 1, 'ones': 1} )
+jobs_to_do.append( {'repeat' :2, 'mod_number': 2, 'mod_pos': 0, 'ones': 1} )
+
+jobs_to_do.append( {'repeat' :3, 'mod_number': 2, 'mod_pos': 1, 'ones': 2} )
+jobs_to_do.append( {'repeat' :3, 'mod_number': 2, 'mod_pos': 0, 'ones': 2} )
+jobs_to_do.append( {'repeat' :3, 'mod_number': 2, 'mod_pos': 1, 'ones': 1} )
+jobs_to_do.append( {'repeat' :3, 'mod_number': 2, 'mod_pos': 0, 'ones': 1} )
+
+
+
+#with ThreadPoolExecutor(max_workers=2) as exe:
+#     result = exe.map(min_coin ,jobs_to_do)
+with Pool(8) as p: 
+    print(p.map(min_coin, jobs_to_do))
+
+#here p does NOT exist
 
 
 
