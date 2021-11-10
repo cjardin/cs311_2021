13e7cfbfa9c53a5b0bdd07fbdea00512d81f2757
Ubuntu <ubuntu@ip-172-31-32-199.us-east-2.compute.internal>
2021-11-10 22:10:24 +0000

    fix bug in line 9

--- assignment4/Node.py
+++ assignment4/Node.py
@@ -6,7 +6,7 @@ NODE_COUNT_PER_LAYER = [4,3,2]
 class Node:
     def __init__(self):
         self.children = []
-        self.node_name = ''.join([random.choice(string.ascii_letters for i in range(3)])
+        self.node_name = ''.join([random.choice(string.ascii_letters) for i in range(3)])
 
      def make_children(self, current_layer_number, node_per_layer_map):
         if current_layer_number >= len(node_per_layer_map):
