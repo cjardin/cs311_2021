050b237ce859c05618cb41c4fe3203688814338d
brianafiore <brianamfiore@gmail.com>
2021-12-11 23:59:19 +0000

    BFS very slow implementation

--- final_project/final.py
+++ final_project/final.py
@@ -1,3 +1,5 @@
+import queue
+
 # import txt file that contains maze
 def import_file(file_name):
     file = open(file_name)
@@ -21,6 +23,29 @@ def start(maze):
             if maze[row][col] == 'o':
                 return row, col
 
+# locate end of maze
+def end(maze, moves):
+    for x, position in enumerate(maze[0]):
+        if position == 'o':
+            start = i
+            x = start
+            y = 0
+            for move in moves:
+                if move == 'LEFT':
+                    x -= 1
+                elif move == 'RIGHT':
+                    x += 1
+                elif move == 'UP':
+                    y -= 1
+                elif move == 'DOWN':
+                    y += 1
+
+            if maze[y][x] == 'x':
+                print('Solution :', moves)
+                move(maze, moves)
+                return True
+            return False
+
 # print formatted maze without commas
 def print_maze(maze):
     for row in maze:
@@ -28,9 +53,69 @@ def print_maze(maze):
             print(cell, end = '')
         print()
 
+# validate cells
+def validate_cells(maze, moves):
+    for i, position in enumerate(maze[0]):
+        if position == 'o':
+            start = i
+            x = start
+            y = 0
+            for move in moves:
+                if move == 'LEFT':
+                    x -= 1
+                elif move == 'RIGHT':
+                    x += 1
+                elif move == 'UP':
+                    y -= 1
+                elif move == 'DOWN':
+                    y += 1
+
+                if not (0 <= x < len(maze[0]) and 0 <= y < len(maze)):
+                    return False
+                elif (maze[y][x] == '#'):
+                    return False
+
+            return True
+
+
+# assign move values
+def move(maze, solution):
+    x = start
+    y = 0
+    position = set()
+    for move in solution:
+        if move == 'LEFT':
+            x -= 1
+        elif move == 'RIGHT':
+            x += 1
+        elif move == 'UP':
+            y -= 1
+        elif move == 'DOWN':
+            y += 1
+        position.add((y, x))
+
+    for y, row in enumerate(maze):
+        for x, col in enumerate(row):
+            if(y, x) in position:
+                print('+ ', end = '')
+            else:
+                print(col + ' ' , end = '')
+        print()
+
 # run program
 maze = import_file("maze.txt")
 maze = format_maze(maze)
+solution = ''
 start = start(maze)
 print_maze(maze)
 print('START: ', start)
+
+moves = queue.Queue()
+moves.put('')
+
+while not end(maze, solution):
+    solution = moves.get() #dequeue
+    for move in ['LEFT', 'RIGHT', 'UP', 'DOWN']:
+        coordinate = solution + move
+        if validate_cells(maze, coordinate):
+            moves.put(coordinate)
